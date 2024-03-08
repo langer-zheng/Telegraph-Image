3137d2df581b82d8d77e98c8b5ccaed3aae67cc6
nijr@citycloud.com.cn <nijr@citycloud.com.cn>
2024-03-08 10:10:03 +0800

    feat: add auth

--- _nuxt/auth.js
+++ _nuxt/auth.js
@@ -4,15 +4,21 @@ var requestOptions = {
   //include authorization credientials
   credentials: "include",
 };
-const authCode = window.localStorage.getItem("authCode") || window.prompt("authCode");
+const authCode =
+  window.localStorage.getItem("authCode") || window.prompt("authCode");
 fetch(`./api/authCode/${authCode}`)
-  .then((res) => {
-    console.log('res',res);
-    window.localStorage.setItem("authCode")
+  .then(async (res) => {
+    console.log("res", res);
+    const { status } = await res.json();
+    if (status === 200) {
+      window.localStorage.setItem("authCode", authCode);
+    } else {
+      console.log("status", status);
+    }
   })
   .catch((e) => {
-    console.log(' ----')
-    console.log('e:', e)
-    console.log(' ----')
+    console.log(" ----");
+    console.log("e:", e);
+    console.log(" ----");
     // window.location.reload();
   });
