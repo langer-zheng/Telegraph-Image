c97a85a700099c6bdd2a224014f82c41e2883acc
nijr@citycloud.com.cn <nijr@citycloud.com.cn>
2024-03-08 10:50:14 +0800

    feat: add auth

--- functions/upload.js
+++ functions/upload.js
@@ -1,22 +1,22 @@
-export async function onRequestPost(context) {
-  // Contents of context object
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context;
-  context.request;
-  const url = new URL(request.url);
-  console.log(' -----------------------')
-  console.log('onRequestPost upload url:', url)
-  console.log(' -----------------------')
-  const response = fetch("https://telegra.ph/" + url.pathname + url.search, {
-    method: request.method,
-    headers: request.headers,
-    body: request.body,
-  });
-  return response;
-}
+// export async function onRequestPost(context) {
+//   // Contents of context object
+//   const {
+//     request, // same as existing Worker API
+//     env, // same as existing Worker API
+//     params, // if filename includes [id] or [[path]]
+//     waitUntil, // same as ctx.waitUntil in existing Worker API
+//     next, // used for middleware or to fetch assets
+//     data, // arbitrary space for passing data between middlewares
+//   } = context;
+//   context.request;
+//   const url = new URL(request.url);
+//   console.log(' -----------------------')
+//   console.log('onRequestPost upload url:', url)
+//   console.log(' -----------------------')
+//   const response = fetch("https://telegra.ph/" + url.pathname + url.search, {
+//     method: request.method,
+//     headers: request.headers,
+//     body: request.body,
+//   });
+//   return response;
+// }
---
+++ functions/upload/[authCode].js
@@ -0,0 +1,39 @@
+export async function onRequestPost(context) {
+    // Contents of context object
+    const {
+      request, // same as existing Worker API
+      env, // same as existing Worker API
+      params, // if filename includes [id] or [[path]]
+      waitUntil, // same as ctx.waitUntil in existing Worker API
+      next, // used for middleware or to fetch assets
+      data, // arbitrary space for passing data between middlewares
+    } = context;
+    context.request;
+    const url = new URL(request.url);
+    console.log(' -----------------------')
+    console.log('onRequestPost upload url:', url)
+    console.log(' -----------------------')
+    if (decodeURIComponent(params.code) === env.AUTH_CODE) {
+        const response = fetch("https://telegra.ph/" + url.pathname + url.search, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+        });
+        return response;
+    }else {
+        return new UnauthorizedException('on auth')
+    }
+  }
+  function UnauthorizedException(reason) {
+    return new Response(reason, {
+      status: 401,
+      statusText: "Unauthorized",
+      headers: {
+        "Content-Type": "text/plain;charset=UTF-8",
+        // Disables caching by default.
+        "Cache-Control": "no-store",
+        // Returns the "Content-Length" header for HTTP HEAD requests.
+        "Content-Length": reason.length,
+      },
+    });
+  }
